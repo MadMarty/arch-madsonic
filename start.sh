ee252baf8d6d0f64f8f6007751a0ac429459ea19
Martin Karel <karelm75@gmail.com>
2015-07-26 13:09:25 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 #create folders on config
-mkdir -p /config/media/incoming
-mkdir -p /config/media/podcasts
-mkdir -p /config/playlists/import
-mkdir -p /config/playlists/export
-mkdir -p /config/playlists/backup
 mkdir -p /config/transcode
 
 #copy transcode to config directory - transcode directory is subdir of path set from --home flag, do not alter
@@ -14,11 +9,11 @@ cp /opt/madsonic/transcode/linux/* /config/transcode/
 # enable/disable ssl based on env variable set from docker container run command
  if [[ $SSL == "yes" ]]; then
         echo "Enabling SSL for Madsonic"
-        /opt/madsonic/madsonic.sh --home=/config --host=0.0.0.0 --https-port=4050 --default-music-folder=/media --default-podcast-folder=/config/media/podcasts --default-playlist-import-folder=/config/playlists/import --default-playlist-export-folder=/config/playlists/export --default-playlist-backup-folder=/config/playlists/backup
+        /opt/madsonic/madsonic.sh --home=/config --host=0.0.0.0 --https-port=4050 --default-music-folder=/media 
  elif [[ $SSL == "no" ]]; then
         echo "Disabling SSL for Madsonic"
-        /opt/madsonic/madsonic.sh --home=/config --host=0.0.0.0 --port=4040 --default-music-folder=/media --default-podcast-folder=/config/media/podcasts --default-playlist-import-folder=/config/playlists/import --default-playlist-export-folder=/config/playlists/export --default-playlist-backup-folder=/config/playlists/backup
+        /opt/madsonic/madsonic.sh --home=/config --host=0.0.0.0 --port=4040 --default-music-folder=/media 
  else
         echo "SSL not defined, defaulting to disabled"
-        /opt/madsonic/madsonic.sh --home=/config --host=0.0.0.0 --port=4040 --default-music-folder=/media --default-podcast-folder=/config/media/podcasts --default-playlist-import-folder=/config/playlists/import --default-playlist-export-folder=/config/playlists/export --default-playlist-backup-folder=/config/playlists/backup
+        /opt/madsonic/madsonic.sh --home=/config --host=0.0.0.0 --port=4040 --default-music-folder=/media 
  fi
