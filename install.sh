1b565ebad1b87b654b02ceeb77509a0334d69917
Martin Karel <karelm75@gmail.com>
2015-08-28 03:47:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ mkdir -p /opt/madsonic/media
 mkdir -p /opt/madsonic/transcode
 
 # download madsonic standalone
-curl -o /opt/madsonic/madsonic.zip -L http://www.madsonic.org/download/6.0/20150725_madsonic-6.0.6890-standalone.zip
+curl -o /opt/madsonic/madsonic.zip -L http://www.madsonic.org/download/6.0/20150827_madsonic-6.0.7120-standalone.zip
 
 # download madsonic transcode 
 curl -o /opt/madsonic/transcode/transcode.zip -L http://www.madsonic.org/download/transcode/20150725_madsonic-transcode-x64.zip
